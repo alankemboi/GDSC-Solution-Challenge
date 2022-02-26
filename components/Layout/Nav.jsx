0e6c77e2ea066ee83b8bf447730f532cad0072ba
Alan Kemboi <alankemboi@gmail.com>
2022-02-26 12:36:31 +0000

    Nav.jsx edited online with Bitbucket

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -106,7 +106,7 @@ const Nav = () => {
           justifyContent="space-between"
         >
           <Link href="/">
-            <a className={styles.logo}>Next.js MongoDB App</a>
+            <a className={styles.logo}>Kisii Atlassian</a>
           </Link>
           <Container>
             {user ? (
